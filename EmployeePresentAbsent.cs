1e3eef4014e17ff3bd25cab3c2125c722861e3c4
aswin26498 <85921826+aswin26498@users.noreply.github.com>
2022-02-27 23:42:50 +0530

    Employee present or absent check

---
+++ EmployeePresentAbsent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFP_day3_PracticeProblems
+{
+    internal class EmployeePresentAbsent
+    {
+        static void Main(string[] args)
+        {
+            //Constants
+            int isFullTime = 1;
+            Random random = new Random();
+
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == isFullTime)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RFP_day3_PracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
